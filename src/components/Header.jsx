7761e6616093536dec89b9f7ba45354400642f82
jamesjo <jamescakr@gmail.com>
2025-03-04 20:10:47 -0700

    completed Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="bg-black text-white text-[20px] flex justify-end h-auto">
+      <div className="flex h-auto mr-8">
+        {/* 현재 font-lato 적용이 안되고있음 */}
+        <button className="px-4 py-6 w-auto hover:text-[#ccc] font-lato">
+          About
+        </button>
+        <button className="px-4 py-6 w-auto hover:text-[#ccc] font-lato">
+          Passion
+        </button>
+        <button className="px-4 py-6 w-auto hover:text-[#ccc] font-lato">
+          Experience
+        </button>
+        <button className="px-4 py-6 w-auto hover:text-[#ccc]">Projects</button>
+        <button className="px-4 py-6 w-auto hover:text-[#ccc]">Contact</button>
+        <button className="px-4 py-6 w-auto hover:text-[#ccc]">Resume</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
