85886354353ab3f278e5302be889b3b37a843c49
jamesjo <jamescakr@gmail.com>
2025-03-06 21:48:57 -0700

    fonts & motion added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,57 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const Header = () => {
+  const containerVariant = {
+    hidden: {},
+    show: {
+      transition: {
+        staggerChildren: 0.1,
+      },
+    },
+  };
+  // 현재 버튼에 모션이 적용 안되는상황. 코드는 작성완료했음
+  const childVariants = {
+    hidden: { opacity: 0, y: -50 },
+    show: {
+      opacity: 1,
+      y: 0,
+    },
+  };
+
   return (
     <div className="fixed top-0 left-0 w-full bg-[#121212] text-[#ccc] text-[20px] flex justify-end h-auto">
-      <div className="flex h-auto mr-8">
-        {/* 현재 font-lato 적용이 안되고있음 */}
-        <button className="px-4 py-6 w-auto hover:text-[#64FFDA] font-lato">
+      <div className="flex h-auto mr-8 font-sf-mono">
+        <motion.button
+          variants={childVariants}
+          className="px-4 py-6 w-auto hover:text-[#64FFDA]"
+        >
           About
-        </button>
-        <button className="px-4 py-6 w-auto hover:text-[#64FFDA] font-lato">
+        </motion.button>
+        <motion.button
+          variants={childVariants}
+          className="px-4 py-6 w-auto hover:text-[#64FFDA]"
+        >
           Passion
-        </button>
-        <button className="px-4 py-6 w-auto hover:text-[#64FFDA] font-lato">
+        </motion.button>
+        <motion.button
+          variants={childVariants}
+          className="px-4 py-6 w-auto hover:text-[#64FFDA]"
+        >
           Experience
-        </button>
-        <button className="px-4 py-6 w-auto hover:text-[#64FFDA]">Projects</button>
-        <button className="px-4 py-6 w-auto hover:text-[#64FFDA]">Contact</button>
+        </motion.button>
+        <motion.button
+          variant={childVariants}
+          className="px-4 py-6 w-auto hover:text-[#64FFDA]"
+        >
+          Projects
+        </motion.button>
+        <motion.button
+          variants={childVariants}
+          className="px-4 py-6 w-auto hover:text-[#64FFDA]"
+        >
+          Contact
+        </motion.button>
       </div>
     </div>
   );
--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -3,40 +3,73 @@ import githubIcon from "../assets/github.png";
 import linkedinIcon from "../assets/linkedin.png";
 import emailIcon from "../assets/email.png";
 import { Download } from "lucide-react";
+import { motion } from "framer-motion";
 
 const Intro = () => {
+  const containerVariant = {
+    hidden: {},
+    show: {
+      transition: {
+        delayChildren: 1,
+        staggerChildren: 0.1,
+      },
+    },
+  };
+
+  const childVariants = {
+    hidden: { opacity: 0, y: -50 },
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: { type: "spring", stiffness: 80, damping: 20 },
+    },
+  };
+
   return (
     <div className="bg-[#121212] text-white h-screen">
       <div className="container mx-auto px-30 w-auto pt-50">
-        <div className="mb-4 text-2xl text-[#64FFDA]">Hi, my name is </div>
-        <div className="text-8xl mb-4">James Jo</div>
-        <div className="text-6xl mb-12">
-          Web Developer specializing in
-          <span className="ml-2 font-bold bg-gradient-to-l from-[#009E75] to-[#64FFDA] text-transparent bg-clip-text">
-            ReactJS
-          </span>
-        </div>
-        <div className="w-3/4 h-auto text-2xl text-[#ccc]">
-          I have a strong foundation in HTML, CSS and JavaScript,
-        </div>
-        <div className="w-3/4 h-auto mb-4 text-2xl text-[#ccc] mb-10">
-          and I’m expanding my expertise with modern web technologies to build
-          impactful solutions.
-        </div>
-        <div className="flex w-128 gap-x-6">
-          <button className="border border-[#64FFDA] text-[#64FFDA] rounded-md w-auto px-4 h-14 flex justify-center items-center text-xl">
-            <Download className="mr-2 text-current" /> Resume
-          </button>
-          <button className="border border-[#ccc] bg-[#ccc] rounded-md w-14 h-14 flex justify-center items-center text-xl transform hover:scale-110 transition duration-300">
-            <img src={emailIcon} alt="email" className="w-7 h-7" />
-          </button>
-          <button className="border border-[#ccc] bg-[#ccc] rounded-md w-14 h-14 flex justify-center items-center text-xl transform hover:scale-110 transition duration-300">
-            <img src={linkedinIcon} alt="LinkedIn" className="w-6 h-6" />
-          </button>
-          <button className="border border-[#ccc] bg-[#ccc] rounded-md w-14 h-14 flex justify-center items-center text-xl transform hover:scale-110 transition duration-300">
-            <img src={githubIcon} alt="GitHub" className="w-6 h-6" />
-          </button>
-        </div>
+        <motion.div variants={containerVariant} initial="hidden" animate="show">
+          <motion.div
+            variants={childVariants}
+            className="mb-4 text-2xl text-[#64FFDA] font-sf-mono"
+          >
+            Hi, my name is
+          </motion.div>
+          <motion.div variants={childVariants} className="text-8xl mb-4 font-inter">
+            James Jo
+          </motion.div>
+          <motion.div variants={childVariants} className="text-6xl mb-12 font-inter">
+            Web Developer specializing in
+            <span className="ml-2 font-bold bg-gradient-to-l from-[#009E75] to-[#64FFDA] text-transparent bg-clip-text">
+              ReactJS
+            </span>
+          </motion.div>
+          <motion.div
+            variants={childVariants}
+            className="w-3/4 h-auto text-2xl text-[#ccc] mb-10 font-lato"
+          >
+            <div>
+              I have a strong foundation in HTML, CSS and JavaScript, and I’m
+              expanding my expertise with modern web technologies to build impactful
+              solutions.
+            </div>
+          </motion.div>
+
+          <motion.div variants={childVariants} className="flex w-128 gap-x-6">
+            <button className="border border-[#64FFDA] text-[#64FFDA] font-sf-mono rounded-md w-auto px-4 h-14 flex justify-center items-center text-xl animate-fadeInDown">
+              <Download className="mr-2 text-current" /> Resume
+            </button>
+            <button className="border border-[#ccc] bg-[#ccc] rounded-md w-14 h-14 flex justify-center items-center text-xl transform hover:scale-110 transition duration-300">
+              <img src={emailIcon} alt="email" className="w-7 h-7" />
+            </button>
+            <button className="border border-[#ccc] bg-[#ccc] rounded-md w-14 h-14 flex justify-center items-center text-xl transform hover:scale-110 transition duration-300">
+              <img src={linkedinIcon} alt="LinkedIn" className="w-6 h-6" />
+            </button>
+            <button className="border border-[#ccc] bg-[#ccc] rounded-md w-14 h-14 flex justify-center items-center text-xl transform hover:scale-110 transition duration-300">
+              <img src={githubIcon} alt="GitHub" className="w-6 h-6" />
+            </button>
+          </motion.div>
+        </motion.div>
       </div>
     </div>
   );
